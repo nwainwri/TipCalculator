e3a5b437486faa2594150b98b0aef78791e8605c
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 22:38:52 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

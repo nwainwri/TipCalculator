0bea5a78487f06d2fed1c62d660595f6105b6080
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-12 13:18:09 -0700

    setup button to calculate tip amounts from appropriate fields; non working currently.

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,16 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+
+@property (weak, nonatomic) IBOutlet UIButton *calculateTipAmountPressed;
+
+- (IBAction) calculateTipPercentage:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -18,6 +28,29 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+- (IBAction) calculateTipPercentage:(id)sender{
+    // create NumberFormatter to format variables as floats and to handle decimal places
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc]init];
+    numberFormatter.locale = [NSLocale currentLocale];// this ensures the right separator behavior (ie, current style for location)
+    numberFormatter.numberStyle = NSNumberFormatterDecimalStyle;
+    [numberFormatter setMaximumFractionDigits:2]; // sets max number of decimal places to two, kinda needed for dollars
+    numberFormatter.usesGroupingSeparator = YES;
+    NSDecimalNumber *billAmount = [[NSDecimalNumber alloc] initWithString:self.billAmountTextField.text]; // converts amount from slider into working float number
+    
+    NSDecimalNumber *tipPercentage = [[NSDecimalNumber alloc] initWithString:self.tipPercentageTextField.text]; // converts text (number) from field into working float number
+    NSDecimalNumber *decimalNumber = [[NSDecimalNumber alloc] initWithInt:10]; // so that I can convert 15 into "0.15" for percentage.
+    tipPercentage = [tipPercentage decimalNumberByDividingBy:decimalNumber];
+    NSDecimalNumber *totalBill = [billAmount decimalNumberByMultiplyingBy:tipPercentage]; // calculation to split amounts.
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"%@", [numberFormatter stringForObjectValue:totalBill]]; // returns value to text field.
+    
+    
+    
+}
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
